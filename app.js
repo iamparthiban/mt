7b16c7dd22a2f622e02a5fbc4c478188bcbaddbf
Parthiban Subramanian <Parthiban@colorado.getebs.com>
2017-03-08 12:37:49 +0530

    added app js file

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module('maze', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "app/views/home.html"
+    })
+    .when("/about", {
+        templateUrl: "app/views/about-us.html"
+    })
+    .when("/contact", {
+        templateUrl: "green.htm"
+    })
+    .when("/seo-services", {
+        templateUrl: "blue.htm"
+    });
+});
